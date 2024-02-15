6b5d2027da1d86236223bbde57c3b89d6f6e0423
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2024-02-15 13:34:37 +0800

    Update website js

---
+++ script.js
@@ -0,0 +1,45 @@
+const toggleButton = document.getElementById('toggle');
+const icon = document.getElementById('icon');
+
+let currentTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
+const applyTheme = (theme) => {
+    document.documentElement.style.setProperty('--background-color', theme === 'dark' ? 'black' : 'white');
+    document.documentElement.style.setProperty('--text-color', theme === 'dark' ? 'white' : 'black');
+    document.documentElement.style.setProperty('--border-color', theme === 'dark' ? 'white' : 'black');
+    document.documentElement.style.setProperty('--icon-color', theme === 'dark' ? 'white' : 'black');
+}
+
+applyTheme(currentTheme); // Apply initial theme
+
+toggleButton.addEventListener('click', () => {
+    if (currentTheme === 'light') {
+        applyTheme('dark');
+        currentTheme = 'dark';
+    } else {
+        applyTheme('light');
+        currentTheme = 'light';
+    }
+});
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    currentTheme = e.matches ? 'dark' : 'light';
+    applyTheme(currentTheme);
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    let parallaxText = document.getElementById("text");
+    let scrollContainer = document.querySelector(".slides");
+  
+    scrollContainer.addEventListener("scroll", () => {
+      let value = scrollContainer.scrollTop;
+      parallaxText.style.marginTop = value * 0.3 + "px";
+  
+      // Adjust the value 100 to the point where you want the text to disappear
+      if (value > 900) {
+        parallaxText.style.opacity = "0";
+      } else {
+        parallaxText.style.opacity = "1";
+      }
+    });
+  });
